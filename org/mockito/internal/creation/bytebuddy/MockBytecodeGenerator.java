020f6b80582ffd3e83a40e7f083dc3de9517d975
Paul Hammant <paul@hammant.org>
2017-06-19 20:41:34 -0400

    2.0.38-beta

--- org/mockito/internal/creation/bytebuddy/MockBytecodeGenerator.java
+++ org/mockito/internal/creation/bytebuddy/MockBytecodeGenerator.java
@@ -7,6 +7,7 @@ import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.MethodTransformer;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.dynamic.loading.MultipleParentClassLoader;
+import net.bytebuddy.dynamic.scaffold.TypeValidation;
 import net.bytebuddy.implementation.FieldAccessor;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.attribute.MethodAttributeAppender;
@@ -29,7 +30,7 @@ class MockBytecodeGenerator {
     private final Random random;
 
     public MockBytecodeGenerator() {
-        byteBuddy = new ByteBuddy();
+        byteBuddy = new ByteBuddy().with(TypeValidation.DISABLED);
         random = new Random();
     }
 
@@ -39,16 +40,19 @@ class MockBytecodeGenerator {
                          .name(nameFor(features.mockedType))
                          .ignoreAlso(isGroovyMethod())
                          .annotateType(features.mockedType.getAnnotations())
-                         .serialVersionUid(42L)
                          .implement(new ArrayList<Type>(features.interfaces))
                          .method(any())
                            .intercept(MethodDelegation.to(DispatcherDefaultingToRealMethod.class))
                            .transform(MethodTransformer.Simple.withModifiers(SynchronizationState.PLAIN))
                            .attribute(MethodAttributeAppender.ForInstrumentedMethod.INSTANCE)
+                         .serialVersionUid(42L)
                          .defineField("mockitoInterceptor", MockMethodInterceptor.class, PRIVATE)
-                         .implement(MockAccess.class).intercept(FieldAccessor.ofBeanProperty())
-                         .method(isHashCode()).intercept(to(MockMethodInterceptor.ForHashCode.class))
-                         .method(isEquals()).intercept(to(MockMethodInterceptor.ForEquals.class));
+                         .implement(MockAccess.class)
+                           .intercept(FieldAccessor.ofBeanProperty())
+                         .method(isHashCode())
+                           .intercept(to(MockMethodInterceptor.ForHashCode.class))
+                         .method(isEquals())
+                           .intercept(to(MockMethodInterceptor.ForEquals.class));
         if (features.crossClassLoaderSerializable) {
             builder = builder.implement(CrossClassLoaderSerializableMock.class)
                              .intercept(to(MockMethodInterceptor.ForWriteReplace.class));
